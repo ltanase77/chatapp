22dde7d0ad58f47ec606aedde79a3c81d71ac652
Lucian Tanase <office@easyconsult.ro>
2018-12-30 22:52:44 +0200

    Adding new custom events: newMessage and createMessage

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+var socket = io();
+    socket.on('connect', function() {
+        console.log('Connected to the server.');
+        socket.emit('createMessage', {
+            from: "Zamol",
+            text: "Hi! What's up guys!",
+        })
+    });
+    socket.on('disconnect', function() {
+        console.log('Disconnected from server!');
+    });
+
+    socket.on('newMessage', function(msg) {
+        console.log('New message received', msg);
+    });
